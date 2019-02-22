bded496ccaf609385c2eaff0618ed5448fb22661
Hannah Rauh <hrauh@umich.edu>
2019-02-22 00:37:15 +0000

    hello world

--- dj4e/dj4e/settings.py
+++ dj4e/dj4e/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gq&bafw+wxmv!ncapqqa@17va-=qf*_4j3pevd(tr-2hj!+z83'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
